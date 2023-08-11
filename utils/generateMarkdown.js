b2f2ea002847758e9929c76301ae9b8b07f2a1c0
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-08-11 11:15:06 -0500

    added links to license badges and info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,13 +2,24 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+const mitBadge = ('https://img.shields.io/badge/License-MIT-yellow.svg');
+const apacheBadge = ('https://img.shields.io/badge/License-Apache_2.0-blue.svg');
+const gplLicense = ('https://img.shields.io/badge/License-GPLv3-blue.svg');
+const iscLicense = ('https://img.shields.io/badge/License-ISC-blue.svg');
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+const mitLink = ('https://opensource.org/licenses/MIT');
+const apacheLink = ('https://opensource.org/licenses/Apache-2.0');
+const gplLink = ('https://www.gnu.org/licenses/gpl-3.0');
+const iscLink = ('https://opensource.org/licenses/ISC');
 function renderLicenseLink(license) {
   
-
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
@@ -17,7 +28,6 @@ function renderLicenseSection(license) {
 
   }
   return result;
-}
 }
 
 // TODO: Create a function to generate markdown for README
