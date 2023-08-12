a137c351c29960a471dbcfea87c733d5c0abf2f8
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-08-12 10:19:19 -0500

    Updated code and testing README format

--- readmegenerator.md
+++ readmegenerator.md
@@ -1,4 +1,4 @@
-# README Generator undefined
+# README Generator https://img.shields.io/badge/License-MIT-yellow.svg
 
   ## Table of Contents
   * [description](#Description)
@@ -10,18 +10,18 @@
   * [contact](#Contact)
 
   ## Description
-  This is a command line application that generates a README file for your projects. It takes input via prompts that are generated using   the inquirer   package. 
+  test
   ## Installation
-  To install this application, create your project and run npm install inquirer from the root folder of your application. 
+  test
   ## Usage
-  To use this application, run your main JS file in node and answer all of the prompts. It will then generate a README file in your directory using the provided template.
+  test
   ## License
-  MIT License
+  https://opensource.org/licenses/MIT
   ## Contributors
-  N/A.
+  test
   ## Tests
-  To test this application, simply run the main JS file in node and enter your desired responses to the prompts, this will generate a file. Once generated you can edit the template to make changes to the appearance of your file. 
+  test
 
   # Contact
-  * GitHub: ChaseOstien
-  * Email: Chaseostien@gmail.com
+  * GitHub: test
+  * Email: test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,60 +3,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-const mitBadge = ('https://img.shields.io/badge/License-MIT-yellow.svg');
-const apacheBadge = ('https://img.shields.io/badge/License-Apache_2.0-blue.svg');
-const gplBadge = ('https://img.shields.io/badge/License-GPLv3-blue.svg');
-const iscBadge = ('https://img.shields.io/badge/License-ISC-blue.svg');
+const mitBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+const apacheBadge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+const gplBadge = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+const iscBadge = 'https://img.shields.io/badge/License-ISC-blue.svg';
 
-function renderLicenseBadge(badge) {
+function renderLicenseBadge(license) {
+  let badge = '';
   switch (renderLicenseBadge) {
-    case data.license = 'MIT License':
+    case license.answers = 'MIT License':
       badge = mitBadge;
       break;
-    case data.license = 'Apache License 2.0':
+    case license.answers = 'Apache License 2.0':
       badge =  apacheBadge;
       break;
-    case data.license = 'GNU General Public License':
+    case license.answers = 'GNU General Public License':
       badge = gplBadge;
       break;
-    case data.license = 'ISC License':
+    case license.answers = 'ISC License':
       badge = iscBadge;
       break;
-    case data.license = 'N/A':
+    case license.answers = 'N/A':
       badge = '';
       break;
     default:
       badge = '';
   }
+  return badge;
 }
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const mitLink = ('https://opensource.org/licenses/MIT');
-const apacheLink = ('https://opensource.org/licenses/Apache-2.0');
-const gplLink = ('https://www.gnu.org/licenses/gpl-3.0');
-const iscLink = ('https://opensource.org/licenses/ISC');
+const mitLink = 'https://opensource.org/licenses/MIT';
+const apacheLink = 'https://opensource.org/licenses/Apache-2.0';
+const gplLink = 'https://www.gnu.org/licenses/gpl-3.0';
+const iscLink = 'https://opensource.org/licenses/ISC';
 
 function renderLicenseLink(license) {
+  license = '';
   switch (renderLicenseLink) {
-    case data.license = 'MIT License':
+    case license.answers = 'MIT License':
       license = mitLink;
       break;
-    case data.license = 'Apache License 2.0':
+    case license.answers = 'Apache License 2.0':
       license =  apacheLink;
       break;
-    case data.license = 'GNU General Public License':
+    case license.answers = 'GNU General Public License':
       license = gplLink;
       break;
-    case data.license = 'ISC License':
+    case license.answers = 'ISC License':
       license = iscLink;
       break;
-    case data.license = 'N/A':
+    case license.answers = 'N/A':
       license = '';
       break;
     default:
       license = '';
   }
+  return license;
 }
 
 // TODO: Create a function that returns the license section of README
@@ -71,7 +74,7 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ${badge}
+  return `# ${data.title} ${mitBadge}
 
   ## Table of Contents
   * [description](#Description)
@@ -89,7 +92,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
   ## License
-  ${data.license}
+  ${mitLink}
   ## Contributors
   ${data.contributors}
   ## Tests
